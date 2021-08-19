753fb38317a3c618830ad374287ae3d151d1f3b8
deepak_rims <deepak.kumar@r-ims.com>
2021-08-19 10:57:09 +0530

    module create

--- src/index.php
+++ src/index.php
@@ -4,15 +4,33 @@ namespace dctest\HelloWorld;
 
 class Index
 {
-    public function Index(){
-        $cmd = @$argv[1];  
-        if($cmd == "make"){
-            $cmdFunction = @$argv[2]; 
-            if($cmdFunction == "module"){
-                $moduleName = @$argv[3]; 
-                $this->module($moduleName);
-            }
+    private $cmd; 
+    private $cmdFunction; 
+    private $moduleName; 
+    function __construct($argv)
+    {
+        $this->cmd = @$argv[1];
+        $this->cmdFunction = @$argv[2];
+        $this->moduleName = @$argv[3];
+        
+        if($this->cmd == "make"){ 
+            $this->make();
         }
+        if($this->cmd == "install"){ 
+            $this->install();
+        }
+    }
+
+    public function install(){ 
+        if($this->cmdFunction == "hmvc"){  
+            $this->install_hmvc();
+        } 
+    }
+    public function make(){ 
+        if($this->cmdFunction == "module"){ 
+            $this->moduleName;
+            $this->module($this->moduleName);
+        } 
     }
     public function module($moduleName = "")
     {
@@ -22,12 +40,18 @@ class Index
         if (!file_exists('application/modules')) {
             mkdir('application/modules', 0777, true);
         }
+        if (!file_exists('application/core')) {
+            mkdir('application/core', 0777, true);
+        }
 
         $moduleName = ucfirst(strtolower($moduleName));
         $moduleFolderPath = 'application/modules/'.$moduleName;
         $moduleControllerPath = 'application/modules/'.$moduleName."/controllers";
         $moduleModelsPath = 'application/modules/'.$moduleName."/models";
         $moduleViewPath = 'application/modules/'.$moduleName."/views";
+        $CTPath = 'application/core';
+        $CTController = "CT_Controller.php";
+        $CTModel = "CT_model.php";
 
         //CREATE MODULES IF NOT EXIST
         if (!file_exists($moduleFolderPath)) {
@@ -54,27 +78,90 @@ class Index
         $viewName = 'index.php';
 
         if(!is_file($moduleControllerPath.'/'.$controllerName)){
-            $contents = $this->controllerData();           // Some simple example content.
+            $contents = $this->controllerData($moduleName);           // Some simple example content.
             file_put_contents($moduleControllerPath.'/'.$controllerName, $contents);     // Save our content to the file.
+        } 
+        if(!is_file($moduleModelsPath.'/'.$modelName)){
+            $contents = $this->modelData($moduleName);           // Some simple example content.
+            file_put_contents($moduleModelsPath.'/'.$modelName, $contents);     // Save our content to the file.
+        } 
+        if(!is_file($moduleViewPath.'/'.$viewName)){
+            $contents = $this->viewData("index");           // Some simple example content.
+            file_put_contents($moduleViewPath.'/'.$viewName, $contents);     // Save our content to the file.
+        } 
+        if(!is_file($CTPath.'/'.$CTController)){
+            $contents = $this->CTControllerData($CTController);           // Some simple example content.
+            file_put_contents($CTPath.'/'.$CTController, $contents);     // Save our content to the file.
+        } 
+        if(!is_file($CTPath.'/'.$CTModel)){
+            $contents = $this->CTModelData($CTModel);           // Some simple example content.
+            file_put_contents($CTPath.'/'.$CTModel, $contents);     // Save our content to the file.
         } 
     }
     function controllerData($name = ""){
         $content = "<?php \n ";
-        $content .= "defined('BASEPATH') or exit('No direct script access allowed');";
-        $content .= "class ".$name." extends CT_Controller";
-        $content .= "{";
-            $content .= "function __construct()";
-            $content .= " {";
-                $content .= "parent::__construct(); ";
-                $content .= "$this->load->model('".$name."_model'); ";
-            $content .= "}";
-            $content .= "function index()";
-            $content .= " {";
-            $content .= "}";
-        $content .= "}";
-        
+        $content .= "defined('BASEPATH') or exit('No direct script access allowed');  \n ";
+        $content .= "class ".$name." extends CT_Controller  \n ";
+        $content .= "{  \n ";
+            $content .= "function __construct()  \n ";
+            $content .= " {  \n ";
+                $content .= "parent::__construct();  \n ";
+                $content .= "\$this->load->model('".$name."_model');  \n ";
+            $content .= "}   \n ";
+            $content .= "function index()  \n ";
+            $content .= " {  \n ";
+            $content .= "}  \n ";
+        $content .= "}  \n ";
+        return $content;
     }
-
+    function modelData($name = ""){
+        $content = "<?php \n ";
+        $content .= "defined('BASEPATH') or exit('No direct script access allowed');";
+        $content .= "class ".$name."_mode extends CT_Model";
+        $content .= "{";
+            $content .= "\tfunction __construct()";
+            $content .= "\t{";
+                $content .= "\t\tparent::__construct(); "; 
+            $content .= "\t}"; 
+        $content .= "}"; 
+        return $content;
+    }
+    function viewData($name = ""){
+        $content = '<!DOCTYPE html> \n ';
+        $content = '<html lang="en"> \n '; 
+            $content .= '\t<head>';  
+                $content .= '\t\t <meta charset="utf-8">';  
+                $content .= '\t\t <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';  
+            $content .= '\t</head>'; 
+            $content .= '\t<body>';  
+            $content .= '\t</body>'; 
+        $content .= '</html>'; 
+        return $content;
+    }
+    function CTControllerData($name = ""){
+        $content = "<?php \n ";
+        $content .= "defined('BASEPATH') or exit('No direct script access allowed');";
+        $content .= "class ".$name." extends CI_Controller";
+        $content .= "{";
+            $content .= "\tfunction __construct()";
+            $content .= "\t{";
+                $content .= "\t\tparent::__construct(); "; 
+            $content .= "\t}"; 
+        $content .= "}"; 
+        return $content;
+    }
+    function CTModelData($name = ""){
+        $content = "<?php \n ";
+        $content .= "defined('BASEPATH') or exit('No direct script access allowed');";
+        $content .= "class ".$name." extends CI_Model";
+        $content .= "{";
+            $content .= "\tfunction __construct()";
+            $content .= "\t{";
+                $content .= "\t\tparent::__construct(); "; 
+            $content .= "\t}"; 
+        $content .= "}"; 
+        return $content;
+    }
 
 }
 
